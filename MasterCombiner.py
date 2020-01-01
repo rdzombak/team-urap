93e474f04a81fd20ea114ce8e65c298e282e5b9d
Alan Jian <alanjian131@berkeley.edu>
2020-01-01 13:58:10 -0500

    Added MasterCombiner.py
    MasterCombiner details how I combined the survey results into one big data set.

---
+++ MasterCombiner.py
@@ -0,0 +1,35 @@
+import GoalCongruenceNLTK as gc
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+E27_1 = gc.pd.read_csv("test_data/First_CheckIn_E27 copy.csv")
+E27_2 = gc.pd.read_csv("test_data/Second_CheckIn_E27 copy.csv")
+E27_f = gc.pd.read_csv("test_data/Final_CheckIn_E27 copy.csv")
+
+H4L_1 = gc.pd.read_csv("test_data/First_Check_H4l copy.csv")
+H4L_2 = gc.pd.read_csv("test_data/Second_Check_H4L copy.csv")
+H4L_f = gc.pd.read_csv("test_data/Final_Check_H4L copy.csv")
+
+lst_of_values = [E27_1, E27_2, E27_f, H4L_1, H4L_2, H4L_f]
+series_of_labeled = pd.Series([])
+for df in lst_of_values:
+    series_of_labeled = series_of_labeled.append(df.iloc[:, 3].dropna())
+
+sns.distplot(series_of_labeled, kde=False, rug=True)
+plt.show()
+
+#Grabbing all the dataframes and forming one big dataframe with necessary data
+lst_of_values = [E27_1, E27_2, E27_f, H4L_1, H4L_2, H4L_f]
+master_data = pd.DataFrame([])
+for n in np.arange(6):
+    series = pd.Series([])
+    for df in lst_of_values:
+        series = series.append(df.iloc[:, n], ignore_index=True)
+    master_data[lst_of_values[0].columns[n]] = series
+master_data = gc.team_renamer(master_data)
+
+master_check = gc.check_accuracy(master_data, gc.cosine_sim(gc.table_by_group(gc.goals_pruner(master_data))))
+sns.lmplot(x='Calculated Values', y='Degree of goal alignment  (1 = lo, 5 = hi)', data=master_check)
+plt.show()
